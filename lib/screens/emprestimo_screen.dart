b6f6d3adbabb2f313ab4f90e0ba61e4fe4cbd887
Aluno <Aluno@politecnico.local>
2026-05-05 14:49:53 -0300

    homescreen impl. parcial

---
+++ lib/screens/emprestimo_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class EmprestimoScreen extends StatefulWidget {
+  const EmprestimoScreen ({super.key});
+
+  @override
+  State<EmprestimoScreen> createState() => _EmprestimoScreenState();
+}
+
+// metodo atualizar emprestimo e devolver
+
+class _EmprestimoScreenState extends State<EmprestimoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/livro_screen.dart
+++ lib/screens/livro_screen.dart
@@ -1,11 +1,76 @@
+import 'package:bibliotecadigital_mobile/core/models/livro.dart';
+import 'package:bibliotecadigital_mobile/core/models/user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class  LivroScreen extends StatelessWidget {
+import '../core/dao/livroDAO.dart';
+
+class  LivroScreen extends StatefulWidget {
   const LivroScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  State<LivroScreen> createState() => _LivroScreenState();
+}
+
+class _LivroScreenState extends State<LivroScreen>{
+  List<Livro> livros = [];
+
+  bool loading = true;
+
+  getLivros() async {
+    loading = true;
+    try {
+      livros = await LivroDao().getLivros();
+    } finally {
+      loading = false;
+    }
+    setState(() {});
+  }
+
+  // criar metodo de emprestimo
+
+  @override
+  void initState() {
+    super.initState();
+    getLivros();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.white,
+      ),
+      body: loading ? Center(child: CircularProgressIndicator(color: Colors.blue))
+          : ListView.builder(
+        itemCount: livros.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('${livros[index].titulo} ${livros[index].anopublicado ?? ''}'),
+                        Text('${livros[index].autor} ${livros[index].categoria ?? ''}'),
+                        Text('${livros[index].n_exemplares} ${livros[index].n_disponiveis ?? ''}'),
+                        // button para realizar emprestimo aqui
+                        // navbar para ver livros que ja foram emprestados
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            )
+          );
+        }
+      )
+    );
   }
 }

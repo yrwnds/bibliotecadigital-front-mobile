80690bcd3db5e50a41f4980836e28d23cf262bd9
Aluno <Aluno@politecnico.local>
2026-05-12 14:54:41 -0300

    implementando tela livros

--- lib/screens/livro_screen.dart
+++ lib/screens/livro_screen.dart
@@ -1,7 +1,13 @@
+import 'package:bibliotecadigital_mobile/core/auth_service.dart';
+import 'package:bibliotecadigital_mobile/core/models/categoria.dart';
 import 'package:bibliotecadigital_mobile/core/models/livro.dart';
+import 'package:bibliotecadigital_mobile/core/models/user.dart';
+
 import 'package:flutter/material.dart';
 
+import '../core/dao/categoriaDAO.dart';
 import '../core/dao/livroDAO.dart';
+import '../core/dao/userDAO.dart';
 
 class  LivroScreen extends StatefulWidget {
   const LivroScreen({super.key});
@@ -12,8 +18,11 @@ class  LivroScreen extends StatefulWidget {
 
 class _LivroScreenState extends State<LivroScreen>{
   List<Livro> livros = [];
+  List<Categoria> categorias = [];
 
   bool loading = true;
+
+
 
   Future<void> getLivros() async {
     loading = true;
@@ -25,26 +34,36 @@ class _LivroScreenState extends State<LivroScreen>{
     setState(() {});
   }
 
-  // criar metodo de emprestimo
+  Future<void> getCategorias() async{
+    loading = true;
+    try {
+      categorias = await CategoriaDao().getCategorias();
+    } finally {
+      loading = false;
+    }
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
     getLivros();
+    getCategorias();
   }
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Livros'),
         backgroundColor: Colors.white,
       ),
       body: loading ? Center(child: CircularProgressIndicator(color: Colors.blue))
           : ListView.builder(
         itemCount: livros.length,
         itemBuilder: (context, index) {
-          return Padding(
+          return
+            Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
             child: Card(
               child: Padding(
@@ -58,8 +77,12 @@ class _LivroScreenState extends State<LivroScreen>{
                         Text('${livros[index].titulo} ${livros[index].anopublicado ?? ''}'),
                         Text('${livros[index].autor} ${livros[index].categoria?.nome ?? ''}'),
                         Text('${livros[index].n_exemplares} ${livros[index].n_disponiveis ?? ''}'),
-                        // button para realizar emprestimo aqui
-                        // navbar para ver livros que ja foram emprestados
+                        TextButton(onPressed: () async {
+                          // logica para emprestimo aqui
+                        },
+                            child: const Text('Pegar emprestado')
+                        )
+               // navbar para ver livros que ja foram emprestados
                       ],
                     ),
                   ],
@@ -68,6 +91,25 @@ class _LivroScreenState extends State<LivroScreen>{
             )
           );
         }
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.book), label: "Livros"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Empréstimos"),
+          ]),
+      drawer: Drawer(
+        child: ListView.builder(
+          itemCount: categorias.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+              child: TextButton(onPressed: () async {
+                // logica para filtrar por categoria aqui
+                 }, child: Text(categorias[index].nome))
+            );
+          }
+        )
       )
     );
   }
